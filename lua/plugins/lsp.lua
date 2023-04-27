c1895011de3a3e1c4ede0cc5f1ffbf321b343bdf
Freddie Haddad <freddie.haddad@outlook.com>
2023-04-27 07:42:54 -0700

    feat(lsp): configure additional lsp servers

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        -- BASH
+        "bash-language-server",
+        "shellcheck",
+
+        -- Go
+        "gopls",
+        "goimports",
+
+        -- Markdown
+        "marksman",
+        "markdownlint",
+
+        -- Python
+        "autopep8",
+        "pyright",
+
+        -- Terraform
+        "terraform-ls",
+        "tflint",
+
+        -- YAML
+        "yamlfmt",
+        "yaml-language-server",
+      })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        -- BASH
+        nls.builtins.formatting.shfmt,
+
+        -- Go
+        nls.builtins.formatting.goimports,
+
+        -- Markdown
+        nls.builtins.diagnostics.markdownlint,
+
+        -- Python
+        nls.builtins.formatting.autopep8,
+
+        -- YAML
+        nls.builtins.formatting.yamlfmt,
+      })
+    end,
+  },
+}
