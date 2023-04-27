7480290d38a811f3bd8cdaad23c278376450e22e
Freddie Haddad <freddie.haddad@outlook.com>
2023-04-27 04:44:22 -0700

    Disable colorschemes bundled with lazyvim
    - Tokyo Night
    - Catppuccin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -7,4 +7,12 @@ return {
     "akinsho/bufferline.nvim",
     enabled = false,
   },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+  {
+    "catppuccin",
+    enabled = false,
+  },
 }
